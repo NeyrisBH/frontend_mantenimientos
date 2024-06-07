e56dcf842de95ede41f2ad1545a479ef31c947f5
Neyris Bohorquez <113575840+NeyrisBH@users.noreply.github.com>
2024-06-07 17:11:05 -0500

    advances Home

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,24 +1,46 @@
 <template>
-    <HeaderComponent/>
+    <HeaderComponent />
+    <SidebarComponent />
+    <section class="main-container">
+        <div class="card-body">
+            <ChartContent />
+        </div>
+        <CardsContent />
+    </section>
 </template>
-  
+
 <script>
-    import HeaderComponent from "../components/HeaderComponent.vue";
+import CardsContent from '@/components/Content/Home/CardsContent.vue';
+import ChartContent from '@/components/Content/Home/ChartContent.vue';
+import HeaderComponent from '@/components/Header/HeaderComponent.vue';
+import SidebarComponent from '@/components/Sidebar/SidebarComponent.vue';
 
-    export default {
-        data() {
-            return {
+export default {
+    data() {
+        return {
 
-            }
-        },
-        components: {
-            HeaderComponent
-        },
-        mounted(){}
-    }
+        }
+    },
+    components: {
+        HeaderComponent,
+        SidebarComponent,
+        ChartContent,
+        CardsContent
+    },
+    methods: {
+    },
+    mounted() { }
+}
 </script>
-  
-<style scoped>
 
+<style scoped>
+.main-container {
+    margin-left: 250px;
+    transition: margin-left 0.5s;
+}
+
+body.sidebar-collapse .main-container {
+    margin-left: 0;
+    width: 100%;
+}
 </style>
-  
